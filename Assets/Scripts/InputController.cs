d5f5f938675d2d035edd6af9c013f45aceff81f8
Jakke Korpelainen <jakke.korpelainen@gmail.com>
2017-06-19 17:00:13 +0300

    Initialize Unity Project. Includes initial camera (#3) and player movement (#4). Closes #2.

---
+++ Assets/Scripts/InputController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputController : MonoBehaviour
+{
+    public float Vertical { get; set; }
+    public float Horizontal { get; set; }
+    public Vector2 MouseInput { get; set; }
+
+    void Update()
+    {
+        Vertical = Input.GetAxis("Vertical");
+        Horizontal = Input.GetAxis("Horizontal");
+        MouseInput = new Vector2(Input.GetAxisRaw("Mouse X"), Input.GetAxisRaw("Mouse Y"));
+    }
+}
---
+++ Assets/Shared/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager
+{
+    public event System.Action<Player> OnLocalPlayerJoined;
+
+    private GameObject gameObject;
+
+    private const string GameObjectName = "_gameManager";
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameManager();
+                _instance.gameObject = new GameObject(GameObjectName);
+                _instance.gameObject.AddComponent<InputController>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private InputController _inputController;
+    public InputController InputController
+    {
+        get
+        {
+            if (_inputController == null)
+                _inputController = gameObject.GetComponent<InputController>();
+
+            return _inputController;
+        }
+    }
+
+    private Player _localPlayer;
+    public Player LocalPlayer
+    {
+        get
+        {
+            return _localPlayer;
+        }
+
+        set
+        {
+            _localPlayer = value;
+            if (OnLocalPlayerJoined != null)
+                OnLocalPlayerJoined(_localPlayer);
+        }
+    }
+}
